2ff21b9143ca24813e5e87ef345fc0b4806cafa2
peter <peterson16@outlook.com.br>
2023-10-23 13:04:34 -0300

    Ranked

---
+++ Ranked.js
@@ -0,0 +1,27 @@
+function calcularNivel(vitorias, derrotas) {
+    const numVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 10 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return `O Herói tem ${numVitorias} vitórias e ${derrotas} derrotas o deixando no ranking ${nivel}`;
+}
+
+let vitorias = 81;
+let derrotas = 32;
+const resultado = calcularNivel(vitorias, derrotas);
+console.log(resultado);
